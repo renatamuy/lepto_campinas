eda9c49f1cb3281a69a42caeb5b24d78bdbe2780
Renata Muylaert <renatamuy@gmail.com>
2024-12-11 14:14:26 +1300

    push

--- lepto_residents.R
+++ lepto_residents.R
@@ -2,31 +2,34 @@
 
 library(ggplot2)
 
-data <- data.frame( Years = 2001:2023,
-  Cases = c(21, 12, 32, 16, 31, 26, 23, 22, 41, 39, 
+df <- data.frame( Years = 2001:2023,
+  Cases = c(21, 12, 32, 16, 31, 26, 23, 22, 41, 19, 
             41, 42, 41, 39, 16, 22, 10, 7, 11, 4, 
             3, 11, 11))
 
-data$highlight <- ifelse(data$Years >= 2007 & data$Years <= 2010, "2007-2010", "Other")
+df$highlight <- ifelse(df$Years >= 2007 & df$Years <= 2010, "2007-2010", "Other")
 
-p <- ggplot(data, aes(x = Years, y = Cases, color = highlight, group = 1)) +
-  geom_line(size = 1.2) +
-  geom_point(size = 4.5) +
+p <- ggplot(df, aes(x = Years, y = Cases, group = 1)) +
+  #geom_vline(xintercept = c(2007, 2010), linetype = "dashed", color = "firebrick", size = 0.8) +  # Dashed lines
+  geom_line(color = "black", size = 1.2) +  
+  geom_point(color = "black", size = 4.5) +  
+  geom_ribbon(data = df[df$Years >= 2007 & df$Years <= 2010, ],
+              aes(x = Years, ymin = -Inf, ymax = Inf), 
+              fill = "firebrick", alpha = 0.3) +  # More transparent area
   geom_label(aes(label = Cases), vjust = -.5, size = 4, label.size = 0, fill = "white") +  
-  scale_color_manual(values = c("2007-2010" = "firebrick", "Other" = "black")) +
   scale_x_continuous(breaks = seq(2001, 2023, by = 1)) +
   labs(title = "", x = "Year", y = "Leptospirosis cases") +
   theme_minimal() +
   theme(legend.position = "none", 
-    axis.title.x = element_text(size = 16),
-    axis.title.y = element_text(size = 16),
-    axis.text.x = element_text(size = 14, angle = 45, hjust = 1),  
-    axis.text.y = element_text(size = 14))
+        axis.title.x = element_text(size = 16),
+        axis.title.y = element_text(size = 16),
+        axis.text.x = element_text(size = 14, angle = 45, hjust = 1),  
+        axis.text.y = element_text(size = 14))
 
 p
 
 setwd('D://OneDrive - Massey University//lepto_campinas//')
 
-ggsave("Leptospirosis_Cases_Per_Year.jpg", plot = p, width =11, height = 7, dpi = 300)
+ggsave("Fig_01.tif", plot = p, width =11, height = 7, dpi = 300)
 
 #-----------------------------------------------------------------------------------------------------------
